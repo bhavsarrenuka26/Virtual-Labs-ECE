e39bb171d97354c8218b9ff963122984792c8d72
bhavsarrenuka26 <bhavsarrenuka12@gmail.com>
2026-05-16 16:52:09 +0530

    Footer Component

--- virtual-lab-ui/src/components/Footer.jsx
+++ virtual-lab-ui/src/components/Footer.jsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className="text-white py-4 mt-auto" style={{ backgroundColor: '#1a2238' }}> {/* Dark Navy Blue */}
+    <footer className="text-white py-4 mt-auto" style={{ backgroundColor: '#1a2238' }}>
       <div className="container">
-        {/* The White Border Box */}
+       
         <div className="border border-white p-4">
           <div className="row">
             
-            {/* Left Column: Contact Info */}
+            {/* conatct info */}
             <div className="col-md-6 mb-3 mb-md-0 d-flex flex-column justify-content-center border-end border-secondary">
               <div className="d-flex align-items-center mb-2">
                 <FaHome className="me-3 text-white-50" />
@@ -26,7 +26,7 @@ const Footer = () => {
               </div>
             </div>
 
-            {/* Right Column: Copyright & Links */}
+            {/* Copyright & Links */}
             <div className="col-md-6 d-flex flex-column justify-content-center ps-md-5">
               <p className="mb-2">© 2026 ECE Dept, PICT | All Rights Reserved</p>
               
