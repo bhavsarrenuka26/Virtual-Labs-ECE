c1e751c77c0ee7f1118648566f41082e25de1372
bhavsarrenuka26 <bhavsarrenuka12@gmail.com>
2026-05-16 16:06:01 +0530

    Assignment model

---
+++ backend/models/Assignment.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const assignmentSchema = new mongoose.Schema({
+  assignmentId: { 
+    type: Number, 
+    required: true 
+
+  },
+ 
+  subjectId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Subject' 
+  },
+  title: { type: String, required: true },
+  shortDesc: { type: String },
+  info: {
+    aim: String,
+    theory: {
+      type: String,
+      default: "" 
+  },
+  },
+  demoVideoUrl: { type: String },
+  quiz: [
+    {
+      questionText: String,
+      options: [String],
+      correctAnswerIndex: Number
+    }
+  ],
+  tryYourself: {
+    link: String
+  },
+  references: [
+    {
+      title: String,
+      url: String
+    }
+  ],
+  labManualLink: String
+});
+
+module.exports = mongoose.model('Assignment', assignmentSchema);
