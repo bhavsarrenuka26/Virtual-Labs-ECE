01ba77bca83294e91d70896c83096ba86155f7db
bhavsarrenuka26 <bhavsarrenuka12@gmail.com>
2026-05-20 23:53:00 +0530

    Solve IPv6 error for Gmail Api

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -225,7 +225,8 @@ router.post('/forgot-password', async (req, res) => {
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-      }
+      },
+      family: 4
     });
 
     const message = `
